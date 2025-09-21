7cb2d7f18190c0d8cdbe7c88cf5aaed88c5a55ad
Mohammed-Jahangir <workjahangir2024@gmail.com>
2025-09-21 12:16:14 +0530

    Added POST /card API to add new cards

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Card Manager Project is running!")
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+cards = []
+
+@app.route("/")
+def home():
+    return "✅ Card Manager API is running!"
+
+@app.route("/cards", methods=["GET", "POST"])
+def manage_cards():
+    if request.method == "POST":
+        data = request.get_json()
+        data["created_at"] = datetime.now().isoformat()
+        cards.append(data)
+        return jsonify({"message": "Card added successfully", "cards": cards}), 201
+
+    return jsonify(cards)
+
+if __name__ == "__main__":
+    app.run(debug=True)
